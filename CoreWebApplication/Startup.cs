3890a5da346e42cc4bc6d7fe9ca32059a8b95df0
AKHIL DHINGRA <dhingra93akhil@gmail.com>
2020-10-11 19:24:14 +0530

    Initial Dot Not Core

--- CoreWebApplication/Startup.cs
+++ CoreWebApplication/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,6 +13,12 @@ namespace CoreWebApplication
 {
     public class Startup
     {
+        private IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -26,13 +33,20 @@ namespace CoreWebApplication
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
+            
+            DefaultFilesOptions defaultFilesOptions = new DefaultFilesOptions();
+            defaultFilesOptions.DefaultFileNames.Clear();
+            defaultFilesOptions.DefaultFileNames.Add("foo.html");
 
+            app.UseDefaultFiles(defaultFilesOptions);
+            app.UseRouting();
+            app.UseStaticFiles();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    // await context.Response.WriteAsync(System.Diagnostics.Process.GetCurrentProcess().ProcessName);
+                    await context.Response.WriteAsync("Hello World");
                 });
             });
         }
