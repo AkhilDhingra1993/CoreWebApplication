6ad17c95d5b13b967160ce56fd37e61225364247
AKHIL DHINGRA <dhingra93akhil@gmail.com>
2022-09-24 20:55:56 +0530

    Introduction to entity framework core
    Install Entity nuget packages and added the DbContext class

---
+++ CoreWebApplication/Models/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreWebApplication.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            :base(options) 
+        {
+
+        }
+        public DbSet<User> User { get; set; }
+    }
+}
